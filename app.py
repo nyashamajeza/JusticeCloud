239ee70677e208c099b7337a935f41828c5e0fc7
nyashamajeza <nyashamajezah@gmail.com>
2026-02-25 13:52:53 +0300

    Fix render port binding

--- app.py
+++ app.py
@@ -220,4 +220,10 @@ def logout():
 
 if __name__ == "__main__":
     init_db()
-    app.run()
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
